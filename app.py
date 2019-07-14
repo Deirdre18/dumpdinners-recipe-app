2b3be6485e70286b01c693e2dbb65d188883e9c4
Ubuntu <ubuntu@ip-172-31-84-14.ec2.internal>
2019-07-14 09:28:32 +0000

    added social icons to footer, fixed add and update category issues but category still needs to be re-selected when updating recipe, made modification to recipe page

--- app.py
+++ app.py
@@ -28,18 +28,29 @@ else:
 mongo = PyMongo(app)
 
 
-
 @app.route('/')
+@app.route('/index')
 def index():
+    """Home page the gets 4 recipes from DB that have been viewed the most"""
+    four_recipes = mongo.db.recipes.find().limit(4)
+    return render_template('allrecipes1.html', recipes=four_recipes)     
+
+@app.route('/allrecipes')
+def allrecipes():
     recipes = mongo.db.recipes.find()
-    return render_template("allrecipes.html", 
-        recipes=mongo.db.recipes.find())
+    return render_template('allrecipes.html', recipes=recipes)    
+    
+@app.route('/allrecipes1')
+def allrecipes1():
+    recipes = mongo.db.recipes.find()
+    four_recipes = mongo.db.recipes.find().limit(4)
+    return render_template('allrecipes1.html', recipes=four_recipes)        
 
 @app.route('/add_recipe')
 def add_recipe():
     recipes = mongo.db.recipes.find()
     return render_template('addrecipe.html',
-        categories=mongo.db.category.find())   
+        categories=mongo.db.categories.find())   
 
     
 @app.route('/insert_recipe', methods=['POST'])
@@ -49,14 +60,6 @@ def insert_recipe():
     flash ("Your recipe has been inserted")
     return redirect(url_for('allrecipes'))
     
-
-
-@app.route('/allrecipes')
-def allrecipes():
-    recipes = mongo.db.recipes.find()
-    return render_template('allrecipes.html', recipes=recipes)
-    
-        
 @app.route('/edit_recipe/<recipe_id>', methods=['GET', 'POST'])
 def edit_recipe(recipe_id):
     """Allows logged in user to edit their own recipes"""
@@ -67,10 +70,8 @@ def edit_recipe(recipe_id):
         return render_template('editrecipe.html', recipe=recipe_db, categories=all_categories)
    
 
-    
-
-
 # referred to this article for advice on 'views' - https://stackoverflow.com/questions/5782311/mongodb-inc-embedded-value-syntax
+
 @app.route('/update_recipe/<recipe_id>', methods=["POST"])
 def update_recipe(recipe_id):
     recipes = mongo.db.recipes
@@ -92,6 +93,8 @@ def update_recipe(recipe_id):
         }
     })
     return redirect(url_for('allrecipes'))
+    
+
 
 @app.route('/delete_recipe/<recipe_id>')
 def delete_recipe(recipe_id):
@@ -130,55 +133,39 @@ def recipe(recipe_id):
 
 #ROUTES FOR CATEGORIES
 
-#Its job is to do a find on the categories table. So categories.html is the template we're going to render. And our categories parameter will feed that from a direct call to MongoDB. So Mongo.db.categories.find.
 
 @app.route('/get_categories')
 def get_categories():
     return render_template('categories.html',
         categories=mongo.db.categories.find())
         
-
-        
-#ADDING ROUTE FOR DELETE TASK
-
-#As a challenge, you could add a new key value property inside the tasks that maybe marks a task as being complete. Then when you display tasks, you'll only display tasks where complete is equal to false. But for now, we'll just delete a task once we're finished with it.. If you do accept the challenge and go on to mark a task as complete, then change the name of the function from delete_task to mark as complete.
-
-#So we access the tasks collection, and we call remove. And we pass in the task_id as the parameter, so it's a very simple function. Again, we use the syntax as we have been using up until now. Key value pair inside the curly braces. We use the object ID to format or parse the task ID in a way that's acceptable to Mongo.  Once that's in place, we want to return or redirect. So we redirect to get tasks. Why? Because once that function is complete, we want to see it disappear. We want visual evidence to see that that task is no longer on our list. So we redirect to the get_tasks function. It's very straightforward. Now, you might need to clear your cache here for this to be picked up. Let's clear the browsing data. As I said earlier, you can use incognito mode if you wish. Click done, and that disappears. So they're marked as complete. As I mentioned, they're simply just deleted from the database. In reality, you would come up with a more sophisticated solution than that. So there we are, task complete.
-
-#------------------------------------- DELETE CATEGORY CORRESPONDING FUNCTION
-
-#create a function (delete_category) and pass in category_id as parameter to remove document from categories collection.
-@app.route('/delete_category/<category_id>')
-def delete_category(category_id):
-    mongo.db.categories.remove({'_id': ObjectId(category_id)})
-    return redirect(url_for('get_categories'))
-
-@app.route('/edit_category/<category_id>')
-def edit_category(category_id):
-    return render_template('editcategory.html',
-    category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
-
-
-
-@app.route('/update_category/<category_id>', methods=['POST'])
-def update_category(category_id):
-    categories = mongo.db.categories.update({'_id': ObjectId(category_id)},
-         {'category_name': request.form.get('category_name')})
-    return redirect(url_for('get_categories'))
-
-
-@app.route('/insert_category', methods=['POST'])
-def insert_category():
-    category_doc = {'category_name': request.form.get('category_name')}
-    mongo.db.categories.insert_one(category_doc)
-    return redirect(url_for('get_categories'))
-
-
 @app.route('/add_category')
 def add_category():
     return render_template('addcategory.html')
     
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+    category_doc = {'category_name': request.form.get('category_name')}
+    mongo.db.categories.insert_one(category_doc)
+    return redirect(url_for('allrecipes'))        
+ 
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editcategory.html',
+    categories=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
 
+
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    categories = mongo.db.categories
+    categories.update({'_id': ObjectId(category_id)},
+    {
+       
+         
+         'category_name': request.form.get('category_name'), 
+        
+    })
+    return redirect(url_for('get_categories'))
 
 #Copied routing for login from Deborah Thompson, student at Code Institute for login routing - #https://github.com/debbiect246/recipe-app#
 
@@ -209,7 +196,7 @@ def register():
         register_id = register.insert_one(request.form.to_dict())
         # print(register)
         object_id = register_id.inserted_id
-        return redirect(url_for('allrecipes',register_id=object_id))
+        return redirect(url_for('allrecipes1',register_id=object_id))
     return render_template('register.html')
               
     
